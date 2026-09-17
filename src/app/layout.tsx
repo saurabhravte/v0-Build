62d480beb6c7241d05dd1c565a41a3d942595a6c
Saurabh Ravte <saurabh.ravte@gmail.com>
2026-09-17 16:13:40 +0530

    Add theme and query providers, implement mode toggle, and update README
    - Introduced ThemeProvider and QueryProvider components for managing themes and queries.
    - Added ModeToggle component for theme switching functionality.
    - Updated README to simplify development server instructions.
    - Added dependencies for react-query and next-themes in package.json and package-lock.json.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "../components/providers/theme-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +28,18 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <QueryProvider>
+            {children}
+          </QueryProvider>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
