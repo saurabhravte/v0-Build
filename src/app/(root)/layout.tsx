214b99a435ffff3c01a54a1b54dca605d71d46a1
Saurabh Ravte <saurabh.ravte@gmail.com>
2026-09-21 12:44:02 +0530

    Implement user and project models, create migration script, and add onboarding functionality

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import { onBoardUser } from "@/features/auth/actions";
+
+export default async function RootGroupLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  await onBoardUser();
+  return children;
+}
